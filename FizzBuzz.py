dca1d66ac8ca18809e5d38509a855ead1f36eca3
Jeferson Morales <61481989+jefergithub@users.noreply.github.com>
2020-05-16 16:24:06 -0500

    Update FizzBuzz.py

--- FizzBuzz.py
+++ FizzBuzz.py
@@ -1,12 +1,25 @@
-def fizz_buzz():
-    for num in range( 1, 101 ):
+# def fizz_buzz():
+#     for num in range( 1, 101 ):
+#         if num % 3 == 0 and num % 5 == 0:
+#             print("FizzBuzz")
+#         elif num % 3 == 0:
+#             print("Fizz")
+#         elif num % 5 == 0:
+#             print("Buzz")
+#         else:
+#             print(num)
+        
+# print(fizz_buzz())
+
+def fizz_buzz(max_num):
+    for num in range(1, max_num + 1):
         if num % 3 == 0 and num % 5 == 0:
-            print("FizzBuzz")
+            print('FizzBuzz')
         elif num % 3 == 0:
-            print("Fizz")
+            print('Fizz')
         elif num % 5 == 0:
-            print("Buzz")
+            print('Buzz')
         else:
             print(num)
-        
-print(fizz_buzz())
+
+fizz_buzz(100)
